057186c5dfcb5a61f0b7b6b7713425a08c124dc3
Fatima Vera <fathy_vera@gmail.com>
2015-07-23 14:03:53 -0400

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -7,5 +8,8 @@ router.get('/', function (req, res) {
         title: 'Quiz'
     });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
